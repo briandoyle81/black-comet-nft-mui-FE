be9ca7620a1dca2d5632f88f2ac5cc274730e207
Brian  Doyle <briandoyle81@gmail.com>
2022-07-15 21:08:39 -0400

    Basic connection to contract

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# black-comet-nft-mui-FE
---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Card, Paper } from '@mui/material';
+import React from 'react';
+import './App.css';
+import GamePanel from './components/game-panel';
+
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk'
+import Web3 from 'web3'
+
+import { AbiItem } from 'web3-utils'
+
+// IMPORTANT:  ABI is in another folder!!!
+// TODO: Move to .env
+import roomTilesContractDeployData from "./RoomTiles.json"
+import { EthereumAddressFromSignedMessageResponse } from '@coinbase/wallet-sdk/dist/relay/Web3Response';
+
+const APP_NAME = 'My Awesome App'
+const APP_LOGO_URL = 'https://example.com/logo.png'
+const DEFAULT_ETH_JSONRPC_URL = 'https://mainnet.infura.io/v3/<YOUR_INFURA_API_KEY>'
+const DEFAULT_CHAIN_ID = 1
+
+// TODO: Move to .env
+const ROOM_TILES_CONTRACT_ADDRESS = "0x034d0C06516D2c2E6ea4Ca1d6d2877722Dc7A685";
+
+
+
+// Initialize Coinbase Wallet SDK
+export const coinbaseWallet = new CoinbaseWalletSDK({
+  appName: APP_NAME,
+  appLogoUrl: APP_LOGO_URL,
+  darkMode: false
+})
+
+// Initialize a Web3 Provider object
+export const ethereum = coinbaseWallet.makeWeb3Provider(DEFAULT_ETH_JSONRPC_URL, DEFAULT_CHAIN_ID)
+
+// Initialize a Web3 object
+export const web3 = new Web3(ethereum as any)
+
+const roomTilesContract = new web3.eth.Contract(roomTilesContractDeployData.abi as AbiItem[], ROOM_TILES_CONTRACT_ADDRESS)
+
+async function GetTilesFromContract() {
+  const res = await roomTilesContract.methods.roomTiles(0).call()
+  console.log(res);
+}
+
+function App() {
+  GetTilesFromContract();
+  GetTilesFromContract()
+  return (
+    <div className="App">
+      <Paper>
+        <Card>
+          <GamePanel />
+          <div>
+            Data
+          </div>
+        </Card>
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
