c9cd67107422053815628e7296f0c1f4fd79ef29
Brian  Doyle <briandoyle81@gmail.com>
2023-07-31 09:30:07 -0400

    Minor improvements to gamelist

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -1,4 +1,12 @@
-import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  List,
+  ListItem,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { ethers } from "ethers";
 import { ReactNode, useEffect, useState } from "react";
@@ -17,55 +25,21 @@ export default function GameList(props: GameListDataInterface) {
   const [games, setGames] = useState<GameInterface[]>([]);
 
   useEffect(() => {
-    console.log("Start of useEffect in GameList");
-
     async function updateGamesFromChain() {
       const gameIds = await props.gameContract_read.extGetGamesOfPlayer(
         props.address
       );
-
       const newGames: GameInterface[] = [];
 
       for (let i = 0; i < gameIds.length; i++) {
         const newGame = await props.gameContract_read.games(gameIds[i]);
-        const {
-          active,
-          denizenTurn,
-          currentPlayerTurnIndex,
-          numPlayers,
-          turnsTaken,
-          eventTracker,
-          mapId,
-          eventPlayerId,
-          eventNumber,
-          eventType,
-          eventPosition,
-          turnTimeLimit,
-          lastTurnTimestamp,
-          denizens,
-        } = newGame;
-
-        const localGame = {
-          active: active,
-          denizenTurn: denizenTurn,
+        newGames.push({
+          ...newGame,
           playerIndexes: await props.gameContract_read.extGetGamePlayerIndexes(
             gameIds[i]
           ),
-          currentPlayerTurnIndex: currentPlayerTurnIndex,
-          numPlayers: numPlayers,
-          turnsTaken: turnsTaken,
-          eventTracker: eventTracker,
-          mapId: mapId,
-          eventPlayerId: eventPlayerId,
-          eventNumber: eventNumber,
-          eventType: eventType,
-          eventPosition: eventPosition,
-          turnTimeLimit: turnTimeLimit,
-          lastTurnTimestamp: lastTurnTimestamp,
-          denizens: denizens,
           gameNumber: gameIds[i],
-        };
-        newGames.push(localGame);
+        });
       }
 
       setGames(newGames);
@@ -77,65 +51,71 @@ export default function GameList(props: GameListDataInterface) {
     }
   }, [gamesLoaded, props.address, props.gameContract_read]);
 
-  function handleGameButtonClick(id: number) {
-    // TODO: Magic number
+  const handleGameButtonClick = (id: number) => {
     localStorage.setItem("lastGame", id.toString());
     localStorage.setItem("lastTab", "2");
     const intId = ethers.BigNumber.from(id).toNumber();
     props.setCurrentGameNumber(intId);
     props.setTabValue(2);
-  }
+  };
 
-  function renderGameData() {
-    const gameList: ReactNode[] = [];
-    games.forEach((game: GameInterface, index: number) => {
-      gameList.push(
-        <Grid item key={index + " Game card"}>
-          <Card>
-            <CardContent>
-              <Typography variant="body1">
-                Game Number: {game.gameNumber.toString()}
-              </Typography>
-              <Typography variant="body1">
-                Active: {game.active.toString()}
-              </Typography>
-              <Typography variant="body1">
-                Players: {game.playerIndexes.toString()}
-              </Typography>
-              <Typography variant="body1">
-                Current Player Turn Index:{" "}
-                {game.currentPlayerTurnIndex.toString()}
-              </Typography>
-              <Typography variant="body1">
-                Turns Taken: {game.turnsTaken.toString()}
-              </Typography>
-              <Typography variant="body1">
-                Map ID: {game.mapId.toString()}
-              </Typography>
-            </CardContent>
-            <Button
-              variant="contained"
-              onClick={() => {
-                handleGameButtonClick(game.gameNumber);
-              }}
-            >
-              Load Game
-            </Button>
-          </Card>
-        </Grid>
-      );
-    });
-    return gameList;
-  }
+  const renderGameData = () => {
+    return games.map((game: GameInterface, index: number) => (
+      <Grid item key={index + " Game card"}>
+        <Card sx={{ mb: 2 }}>
+          <CardContent>
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              Game Number: {game.gameNumber.toString()}
+            </Typography>
+            <List>
+              <ListItem>
+                <Typography variant="body1">
+                  Active: {game.active.toString()}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  Players: {game.playerIndexes.toString()}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  Current Player Turn Index:{" "}
+                  {game.currentPlayerTurnIndex.toString()}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  Turns Taken: {game.turnsTaken.toString()}
+                </Typography>
+              </ListItem>
+              <ListItem>
+                <Typography variant="body1">
+                  Map ID: {game.mapId.toString()}
+                </Typography>
+              </ListItem>
+            </List>
+          </CardContent>
+          <Button
+            variant="contained"
+            onClick={() => handleGameButtonClick(game.gameNumber)}
+            sx={{ m: 2 }}
+          >
+            Load Game
+          </Button>
+        </Card>
+      </Grid>
+    ));
+  };
 
   return (
-    <Box>
-      <Typography variant="body1">
+    <Box sx={{ mt: 3 }}>
+      <Typography variant="h5" sx={{ mb: 2 }}>
         Number of Games In: {games.length}
       </Typography>
-      <Box>
-        <Grid container>{renderGameData()}</Grid>
-      </Box>
+      <Grid container spacing={2}>
+        {renderGameData()}
+      </Grid>
     </Box>
   );
 }
